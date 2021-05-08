44f9b719547994e7f89b6d4f90b3b392192c4f96
Catalina Arriagada Guerrero <75139474+catalina-arriagada@users.noreply.github.com>
2021-05-07 20:04:36 -0400

    Paginita-commit1

---
+++ js/app.js
@@ -0,0 +1,10 @@
+var eres = prompt("¿Eres la Cata?");
+
+if (eres == "si", "shi", "claro","por supuesto", "porsupollo") {
+    alert ("Bienvenida :D!");
+}
+else {
+    alert("Chao chao Bye bye")
+    alert("Ño mentira, no puedo sacarte de la página... aun")
+
+}
